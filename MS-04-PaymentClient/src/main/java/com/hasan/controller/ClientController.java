0f6fe2e62a33824b272c738303642b777a96fdb4
Mohd Hasan <125153993+HasanN-tech@users.noreply.github.com>
2026-08-31 19:31:12 +0530

    Add files via upload

---
+++ MS-04-PaymentClient/src/main/java/com/hasan/controller/ClientController.java
@@ -0,0 +1,27 @@
+package com.hasan.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ClientController {
+
+	@Autowired
+	private DiscoveryClient client;
+	
+	@GetMapping("/msg")
+	public String paymentClient() {
+		List<ServiceInstance> instances = client.getInstances("MS-04-PaymentService");
+		ServiceInstance serviceInstance = instances.get(0);
+		String url=serviceInstance.getUri()+"/payment";
+		return new RestTemplate().getForObject(url, String.class);
+		
+	}
+	
+}
