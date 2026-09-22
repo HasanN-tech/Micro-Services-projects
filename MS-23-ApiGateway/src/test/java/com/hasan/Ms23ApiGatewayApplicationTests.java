7a670f2b37c4f3cdce9467d34dca9408dade65b7
Mohd Hasan <125153993+HasanN-tech@users.noreply.github.com>
2026-09-22 12:03:57 +0530

    Add files via upload

---
+++ MS-23-ApiGateway/src/test/java/com/hasan/Ms23ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms23ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
